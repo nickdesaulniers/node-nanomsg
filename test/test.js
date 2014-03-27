a925cf46b9075ed00e61c5cd801c7c467e7bc37e
Tim Cameron Ryan <id@timryan.org>
2014-03-27 13:32:37 -0400

    Adds test for request/reply.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+// http://tim.dysinger.net/posts/2013-09-16-getting-started-with-nanomsg.html
+
 var assert = require('assert');
 var should = require('should');
 var nano = require('../');
@@ -12,13 +14,13 @@ test('bindings should exist', function (t) {
 });
 
 
-test('pubsub test', function (t) {
+test('socket pub sub', function (t) {
   t.plan(1);
 
   var pub = nano.socket('pub');
   var sub = nano.socket('sub');
 
-  var addr = 'inproc://a';
+  var addr = 'inproc://pubsub';
   var msg = 'hello world';
 
   pub.bind(addr);
@@ -37,13 +39,13 @@ test('pubsub test', function (t) {
 });
 
 
-test('pair test', function (t) {
+test('socket pairs', function (t) {
   t.plan(1);
 
   var s1 = nano.socket('pair');
   var s2 = nano.socket('pair');
 
-  var addr = 'inproc://b';
+  var addr = 'inproc://pairs';
   var msg = 'hello world';
 
   s1.bind(addr);
@@ -62,13 +64,44 @@ test('pair test', function (t) {
 });
 
 
+test('socket req rep', function (t) {
+  t.plan(2);
+
+  var req = nano.socket('req');
+  var rep = nano.socket('rep');
+
+  var addr = 'inproc://reqrep';
+  var msg1 = 'knock knock';
+  var msg2 = 'whose there?'
+
+  rep.bind(addr);
+  req.connect(addr);
+
+  rep.on('message', function (buf) {
+    t.equal(buf.toString(), msg1, 'request received');
+    rep.send(msg2);
+  });
+
+  req.on('message', function (buf) {
+    t.equal(buf.toString(), msg2, 'reply received');
+
+    req.close();
+    rep.close();
+  });
+
+  setTimeout(function () {
+    req.send(msg1);
+  }, 100);
+});
+
+
 test('multiple binds on same address', function (t) {
   t.plan(1);
 
   var s1 = nano.socket('pair');
   var s2 = nano.socket('pair');
 
-  var addr = 'inproc://c';
+  var addr = 'inproc://multiplebinds';
 
   s2.on('error', function (err) {
     t.ok(err, 'error was thrown.')
