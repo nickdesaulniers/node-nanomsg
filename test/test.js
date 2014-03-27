40edfc0a807944e30ad369cd3db6a1ae4739a062
Tim Cameron Ryan <id@timryan.org>
2014-03-27 13:57:44 -0400

    Adds example for bus.

--- test/test.js
+++ test/test.js
@@ -94,6 +94,66 @@ test('socket req rep', function (t) {
   }, 100);
 });
 
+test('socket bus', function (t) {
+  // http://250bpm.com/blog:17
+
+  // Number of buses to create.
+  var count = 3;
+  var total = count * (count-1), current = 0;
+  t.plan(count);
+
+  // Create buses.
+  var buses = {};
+  for (var i = 0; i < count; i++) {
+    (function (i) {
+      var bus = nano.socket('bus');
+      var addr = 'inproc://bus' + i;
+      bus.bind(addr);
+      buses[addr] = bus;
+
+      // Add a "response count" for each bus.
+      // We want this to equal the number of other buses.
+      bus.responseCount = 0;
+
+      // Tally messages from other buses.
+      bus.on('message', function (msg) {
+        console.error('#', 'received message from', msg.toString(), 'on', addr)
+        this.responseCount++;
+        current++;
+        if (this.responseCount == count - 1) {
+          // All set! bus received all messages.
+          t.ok(true, 'all messages received on ' + addr);
+        }
+        if (current == total) {
+          // close all buses.
+          Object.keys(buses).forEach(function (addr) {
+            buses[addr].close();
+          })
+        }
+      })
+    })(i);
+  }
+
+  // Connect all possible pairs of buses.
+  setTimeout(function () {
+    var keys = Object.keys(buses)
+    for (var i = 0; i < keys.length; i++) {
+      for (var j = i+1; j < keys.length; j++) {
+        console.error('#', 'connecting', keys[i], 'to', keys[j]);
+        buses[keys[i]].connect(keys[j])
+      }
+    }
+  }, 500);
+
+  // Send messages on every bus.
+  setTimeout(function () {
+    Object.keys(buses).forEach(function (addr) {
+      console.error('#', 'writing on', addr, addr);
+      buses[addr].send(addr);
+    })
+  }, 1000);
+});
+
 
 test('multiple binds on same address', function (t) {
   t.plan(1);
@@ -104,7 +164,7 @@ test('multiple binds on same address', function (t) {
   var addr = 'inproc://multiplebinds';
 
   s2.on('error', function (err) {
-    t.ok(err, 'error was thrown.')
+    t.ok(err, 'error was thrown on multiple binds.')
 
     s1.close();
     s2.close();
