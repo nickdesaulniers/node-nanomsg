b65d7b71ec197df57240fedfa1a0d543771ae7d6
Ant Skelton <ant@ant.org>
2014-04-03 02:46:02 +0100

    add send.js test, fix send bug

---
+++ test/send.js
@@ -0,0 +1,29 @@
+// https://github.com/chuckremes/nn-core/blob/master/spec/nn_send_spec.rb
+
+var assert = require('assert');
+var should = require('should');
+var nano = require('../');
+var nn = nano._bindings;
+
+var test = require('tape');
+
+
+test('send returns number of bytes sent for bound socket', function(t) {
+    t.plan(1);
+
+    var sock = nano.socket('pub');
+    sock.bind('inproc://some_address');
+    var rc = sock.send('ABC');
+    t.equal(rc, 3);
+    sock.close();
+});
+
+test('send returns number of bytes queued for unbound socket', function(t) {
+    t.plan(1);
+
+    var sock = nano.socket('pub');
+    var rc = sock.send('ABC');
+    t.equal(rc, 3);
+    sock.close();
+});
+
