727f552fd0ec095c3beb02c235721e47a7e931f7
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-03-25 22:05:04 -0700

    test: null terminated strings PR: #75

--- test/send.js
+++ test/send.js
@@ -25,3 +25,73 @@ test('send returns number of bytes queued for unbound socket', function (t) {
     sock.close();
 });
 
+test('send can take a string', function (t) {
+  t.plan(3);
+
+  var pub = nano.socket('pub');
+  var sub = nano.socket('sub');
+  var addr = 'inproc://some_address';
+  var msg = 'hi';
+
+  pub.bind(addr);
+  sub.connect(addr);
+
+  sub.on('message', function (buf) {
+    t.equal(buf.toString(), msg);
+    t.equal(buf.length, msg.length);
+    pub.close();
+    sub.close();
+  });
+
+  var bytes = pub.send(msg);
+  t.equal(bytes, msg.length);
+});
+
+test('send can take a buffer', function (t) {
+  t.plan(3);
+
+  var pub = nano.socket('pub');
+  var sub = nano.socket('sub');
+  var addr = 'inproc://some_address';
+  var msg = new Buffer('hello');
+
+  pub.bind(addr);
+  sub.connect(addr);
+
+  sub.on('message', function (buf) {
+    t.equal(buf.toString(), msg.toString());
+    t.equal(buf.length, msg.length);
+    pub.close();
+    sub.close();
+  });
+
+  var bytes = pub.send(msg);
+  t.equal(bytes, msg.length);
+});
+
+test('should not null terminate when sending strings', function (t) {
+  t.plan(3);
+
+  var pub = nano.socket('pub');
+  var sub = nano.socket('sub');
+  var addr = 'inproc://some_address';
+  var msg = 'hello';
+
+  pub.bind(addr);
+  sub.connect(addr);
+
+  sub.on('message', function (buf) {
+    if (buf[buf.length - 1] === 0) {
+      t.fail();
+    }
+    t.equal(buf.equals(new Buffer(msg)), true);
+    t.equal(buf.length, msg.length);
+    pub.close();
+    sub.close();
+  });
+
+  var bytes = pub.send(msg);
+  t.equal(bytes, msg.length);
+
+});
+
