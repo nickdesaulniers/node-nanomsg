a956f9484ccd4e19bf9d144c74f8f7b5ec207b6c
Comick <comick@autistici.org>
2015-04-11 11:10:09 +0200

    Added option to use buffer or string message for per fetsts.

--- perf/remote_thr.js
+++ perf/remote_thr.js
@@ -2,6 +2,7 @@
 
 var nano = require('../');
 var assert = require('assert');
+var createMsg = require('./common').createMsg;
 
 if (process.argv.length != 5) {
     console.log('usage: node remote_thr.js <bind-to> <msg-size> <msg-count>');
@@ -11,14 +12,15 @@ if (process.argv.length != 5) {
 var connect_to = process.argv[2];
 var sz = Number(process.argv[3]);
 var count = Number(process.argv[4]);
+// Specific to node-nanomsg.
+var msgType = process.argv[5] || '--buffer';
 
 var s = nano.socket('pair');
 assert(s.binding !== -1);
 var rc = s.connect(connect_to);
 assert(rc >= 0);
 
-var buf = new Buffer(sz);
-buf.fill('o');
+var buf = createMsg(msgType, sz);
 
 for (var i = 0; i != count; i++) {
     var nbytes = s.send(buf);
