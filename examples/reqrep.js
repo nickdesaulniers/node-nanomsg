425482c0e2779842c2cf123bbe87dfa79f78f38b
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-06-01 15:00:27 -0700

    add example for req/rep pair PR: #94

---
+++ examples/reqrep.js
@@ -0,0 +1,23 @@
+var nano = require('../');
+
+var rep = nano.socket('rep');
+var req = nano.socket('req');
+
+var addr = 'tcp://127.0.0.1:5555';
+
+rep.bind(addr);
+req.connect(addr);
+
+rep.on('message', function (buf) {
+  console.log('received request: ', buf.toString());
+  rep.send('world');
+});
+
+req.on('message', function (buf) {
+  console.log('received response: ', buf.toString());
+  req.close();
+  rep.close();
+});
+
+req.send('hello');
+
