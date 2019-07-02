668a1671c13aa9cf8aa25ae362c775ce9771ed3b
Nick Desaulniers <nickdesaulniers@users.noreply.github.com>
2019-07-01 19:42:09 -0700

    support node v12 (#211)
    * support node v12
    - upgrade nan to v2.14
    - use  Nan::Utf8String rather than v8::String::Utf8Value
    
    Fixes #206
    
    * fix 1 of the 2 Nan::Callback::Call deprecation warnings
    
    Looks like the Nan::Callback::Call() API deprecated bare calls that
    don't pass a new Nan::AsyncResource().  Luckily, in this case we were
    already subclassing Nan::AsyncWorker, which has a member named
    `async_resource` so all we have to do is explicitly pass this along.
    
    I wonder if Nan should just do this for me? *hmm*
    
    Bug #209

--- src/node_nanomsg.cc
+++ src/node_nanomsg.cc
@@ -64,7 +64,7 @@ NAN_METHOD(Chan) {
   int s = Nan::To<int>(info[0]).FromJust();
   int level = NN_SUB;
   int option = Nan::To<int>(info[1]).FromJust();
-  String::Utf8Value str(info[2]);
+  Nan::Utf8String str(info[2]);
 
   info.GetReturnValue().Set(
       Nan::New<Number>(nn_setsockopt(s, level, option, *str, str.length())));
@@ -72,14 +72,14 @@ NAN_METHOD(Chan) {
 
 NAN_METHOD(Bind) {
   int s = Nan::To<int>(info[0]).FromJust();
-  String::Utf8Value addr(info[1]);
+  Nan::Utf8String addr(info[1]);
 
   info.GetReturnValue().Set(Nan::New<Number>(nn_bind(s, *addr)));
 }
 
 NAN_METHOD(Connect) {
   int s = Nan::To<int>(info[0]).FromJust();
-  String::Utf8Value addr(info[1]);
+  Nan::Utf8String addr(info[1]);
 
   info.GetReturnValue().Set(Nan::New<Number>(nn_connect(s, *addr)));
 }
@@ -99,7 +99,7 @@ NAN_METHOD(Send) {
     info.GetReturnValue().Set(Nan::New<Number>(nn_send(
         s, node::Buffer::Data(info[1]), node::Buffer::Length(info[1]), flags)));
   } else {
-    String::Utf8Value str(info[1]);
+    Nan::Utf8String str(info[1]);
     info.GetReturnValue().Set(
         Nan::New<Number>(nn_send(s, *str, str.length(), flags)));
   }
@@ -230,7 +230,7 @@ public:
 
     Local<Value> argv[] = { Nan::New<Number>(err) };
 
-    callback->Call(1, argv);
+    callback->Call(1, argv, async_resource);
   };
 
 private:
