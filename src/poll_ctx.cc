8296b43331bef47e1e4c2cae1eb32880f04d1d8a
Nick Desaulniers <nick.desaulniers@gmail.com>
2019-07-04 15:41:34 -0700

    underscore suffix private members

--- src/poll_ctx.cc
+++ src/poll_ctx.cc
@@ -6,11 +6,12 @@ void PollCtx::on_readable(uv_poll_t* req, int /* status */, int events) {
     return;
   Nan::HandleScope scope;
   v8::Local<v8::Value> argv[] = { Nan::New<v8::Number>(events) };
-  reinterpret_cast<PollCtx*>(req->data)->callback.Call(1, argv);
+  reinterpret_cast<PollCtx*>(req->data)->callback_.Call(1, argv);
 }
 
 void PollCtx::begin_poll (const int s, const bool is_sender) {
   size_t siz = sizeof(uv_os_sock_t);
+  uv_os_sock_t sockfd;
   nn_getsockopt(s, NN_SOL_SOCKET, is_sender ? NN_SNDFD : NN_RCVFD, &sockfd,
       &siz);
   if (sockfd != 0) {
@@ -20,7 +21,7 @@ void PollCtx::begin_poll (const int s, const bool is_sender) {
 }
 
 PollCtx::PollCtx (const int s, const bool is_sender,
-    const v8::Local<v8::Function> cb): callback(cb) {
+    const v8::Local<v8::Function> cb): callback_(cb) {
   // TODO: maybe container_of can be used instead?
   // that would save us this assignment, and ugly static_cast hacks.
   poll_handle.data = this;
--- src/poll_ctx.h
+++ src/poll_ctx.h
@@ -3,15 +3,13 @@
 #include <nan.h>
 
 class PollCtx {
-  private:
-    const Nan::Callback callback;
-    uv_os_sock_t sockfd; // for libnanomsg
-    void begin_poll (const int s, const bool is_sender);
-    static void on_readable(uv_poll_t* req, int /* status */, int events);
-  public:
-    uv_poll_t poll_handle; // for libuv
-    PollCtx (const int s, const bool is_sender,
-        const v8::Local<v8::Function> cb);
-    static v8::Local<v8::Value> WrapPointer (void* ptr, size_t length);
-    static PollCtx* UnwrapPointer (v8::Local<v8::Value> buffer);
+  const Nan::Callback callback_;
+  void begin_poll (const int s, const bool is_sender);
+  static void on_readable(uv_poll_t* req, int /* status */, int events);
+public:
+  uv_poll_t poll_handle; // for libuv
+  PollCtx (const int s, const bool is_sender,
+      const v8::Local<v8::Function> cb);
+  static v8::Local<v8::Value> WrapPointer (void* ptr, size_t length);
+  static PollCtx* UnwrapPointer (v8::Local<v8::Value> buffer);
 };
